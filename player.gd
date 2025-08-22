a2009e135912223d27c177d7d8144f34a38dd87b
Bryan Oyarzo <thebroyyt@gmail.com>
2025-08-22 19:58:46 -0400

    Actualización: Nueva mecánica añadida

--- player.gd
+++ player.gd
@@ -1,21 +1,48 @@
 extends CharacterBody2D
+
+#region Declaración de Variables
 # Variables de movimiento --------------------------------------------------------------------------
 const speed = 200
 var direction : Vector2 # Vector 2 significa 2D, conjunto de coordenadas "(x,y)"
+
 # Maquina de Estados -------------------------------------------------------------------------------
 enum Estados {IDLE, WALK, RUN}
 var estado_actual: Estados
+
 # Variables de animación ---------------------------------------------------------------------------
 @onready var player_animation: AnimatedSprite2D = $AnimatedSprite2D
 var direccion_horizontal: Vector2
 
+# Variable para poder tomar items ------------------------------------------------------------------
+@onready var item: Node2D = $"../Item"
+@onready var test_level: Node = $".."
+var item_en_mano : Node = null # Null significa que no se tiene nada en la mano 
+var distancia_umbral = 25.0
+
+# Variables para la dirección a la que mira el jugador ---------------------------------------------
+var direccion_mira: Vector2 = Vector2.RIGHT
+#endregion 
+
+func _process(delta: float) -> void:
+	var input_dir = Vector2.ZERO
+	
+	input_dir.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	if input_dir != Vector2.ZERO:
+		direccion_mira = input_dir.normalized()
+	
+	var distancia = global_position.distance_to(item.global_position)
+	if distancia < distancia_umbral and Input.is_action_just_pressed("take_item") and item_en_mano == null:
+		tomar_item(item)
+	elif distancia < distancia_umbral and Input.is_action_just_pressed("take_item") and item_en_mano == item:
+		soltar_item(item)
+		
 func _physics_process(delta: float) -> void:
 	
 	## Movimientos del Jugador en 4 Direcciones ----------------------------------------------------
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	actualizar_estado()
 	move_and_slide()
-	
+
 #---------------------------------------------------------------------------------------------------
 # La función actualizar_estado permite crear las conexiones entre los estados, por ejemplo pasar
 # del estado IDLE a WALK o a RUN, es cómo el sistema de redes de la maquina de estados.
@@ -62,3 +89,18 @@ func actualizar_estado():
 	
 func cambiar_estado(nuevo_estado : Estados):
 	estado_actual = nuevo_estado
+	
+func tomar_item(item: Node2D):
+	item_en_mano = item
+	item.get_parent().remove_child(item) # Hace que el item deje de estar en el "suelo" y se una a ti
+	$HandPosition.add_child(item)
+	item.position = Vector2.ZERO
+	
+func soltar_item(item: Node2D):
+	if item_en_mano:
+		var distancia_objeto = direccion_mira * 16 # Distancia de objeto delante del jugador
+		item.get_parent().remove_child(item)
+		test_level.add_child(item)
+		item_en_mano.global_position = global_position + distancia_objeto
+		# Aquí el item vuelve a estar tirado
+		item_en_mano = null
